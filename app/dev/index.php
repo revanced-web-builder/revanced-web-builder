9839a7e1c6dce1468b3a82dbfd1b39429adb1639
revanced-web-builder <35475204+revanced-web-builder@users.noreply.github.com>
2022-09-29 03:39:41 -0800

    GUI in /dev/index.html for creating config.json.dist

--- app/dev/index.php
+++ app/dev/index.php
@@ -8,35 +8,40 @@ $newVersion = "0.1.0929"; // version being packaged (make sure to change this)
 
 require_once("../functions.php");
 
-
-
-
 $query = (isset($_GET['q']) && $_GET['q'] != "") ? $_GET['q'] : false;
 $debug = new Debug();
 
+if ($query == "configCreate") {
 
-if ($query == "create") {
+  $version = $_GET['version'];
 
   // We need to create a config file from scratch
-  echo $debug->generate(); // this will combine all default config files from the dev/ directory into a valid config.json file in /app/
+  echo $debug->configCreate($version); // this will combine all default config files from the dev/ directory into a valid config.json file in /app/
 
 }
+?>
 
+<h1>ReVanced Web Builder: Dev Tools</h1>
 
+<h2>Create config.json.dist</h2>
+<form method="get" action="index.php">
+  <input type="hidden" name="q" value="configCreate" />
+  <p>Version: <input name="version" size="8" type="text" /> <input type="submit" value="Create" /></p>
+</form>
 
-
+<?php
 // Debug extends the Config class because it mostly manipulates the config file and needs the read/write functions of it
 class Debug {
 
   // Combine all config files in app/dev/ to create a valid config.json.dist file in /app/
-  function generate() {
+  function configCreate($version) {
 
     $configSrc = Files::read("dev/config.config.json"); // all paths start at the /app/ folder. so ../ will be the apps folder
     $appSrc = Files::read("dev/config.apps.json");
     $toolSrc = Files::read("dev/config.tools.json");
     $themeSrc = Files::read("dev/config.themes.json");
 
-    $configs = ["config" => $configSrc, "themes" => $themeSrc, "apps" => $appSrc, "tools" => $toolSrc, "version" => $GLOBALS['newVersion'], "versionLast" => $GLOBALS['newVersion']];
+    $configs = ["config" => $configSrc, "themes" => $themeSrc, "apps" => $appSrc, "tools" => $toolSrc, "version" => $version, "versionLast" => $version];
 
     Files::write("config.json.dist", json_encode($configs, JSON_PRETTY_PRINT));
 
