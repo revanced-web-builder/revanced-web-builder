6bdd9f665947e3b1741fd50f62f44197f8c0167c
revanced-web-builder <35475204+revanced-web-builder@users.noreply.github.com>
2022-10-05 03:12:22 -0800

    Dev Tools back/logout buttons

--- app/dev/index.php
+++ app/dev/index.php
@@ -181,11 +181,33 @@ if ($query == "deleteconfigdist") {
     padding-left: 25px;
   }
 
+  #adminBack {
+    position: absolute;
+    top: 2%;
+    right: 2%;
+    cursor: pointer;
+  }
+
+
   </style>
 
 </head>
 
 <body>
+
+<!-- Back button -->
+<div id="adminBack" title="Back to Admin Panel">
+  <a href="../admin.php" title="Back to Admin panel">
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+  </svg></a>
+
+  <a id="adminLogout" href="../admin.php?logout" title="Logout" class="ms-2">
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+      <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+      <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
+    </svg></a>
+</div>
 
 <?php
 if (!$auth->valid) {
